bc9a8a3e2845630da484d63efc22a79c22abe024
Enrico Robles del Rio <enrico_rr@icloud.com>
2021-09-25 12:35:10 -0500

    adding files to new branch to create markers

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -14,11 +14,24 @@ let map = L.map("mapid", {
   });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+
+/// Create a light-yellow circle with black lines indicating a 300-meter radius of Central Los Angeles on a dark map.
+/// Colors: https://www.color-hex.com/color/fff2cc
+L.circleMarker([34.0522, -118.2437], {
+  color: "black",
+  fillcolor: '#ffd966',
+  fillOpacity: 0.5,
+  radius: 300
+}).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
