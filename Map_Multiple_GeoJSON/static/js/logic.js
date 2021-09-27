2f53a69e95a63c6a54d50f492cd4eee0d75f4cab
Enrico Robles del Rio <enrico_rr@icloud.com>
2021-09-26 19:05:58 -0500

    adding GeoJSON map code

--- Map_Multiple_GeoJSON/static/js/logic.js
+++ Map_Multiple_GeoJSON/static/js/logic.js
@@ -9,20 +9,6 @@ let map = L.map("mapid", {
     zoom: 2
   });
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";  
-
-// Grabbing our GeoJSON data.
-L.geoJson(sanFranAirport, {
-  // We turn each feature into a marker on the map.
-  onEachFeature: function(feature, layer) {
-    console.log(layer);
-    layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2><hr></hr><h3> Airport name: " + feature.properties.name + "</h3>");
-  }
-
-}).addTo(map);
-
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -31,4 +17,20 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 });
 
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+streets.addTo(map);  
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/ENRICOROBLESDELRIO/Mapping_Earthquakes/main/Map_Multiple_GeoJSON/Resources/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+  onEachFeature: function(feature, layer) {
+  console.log(layer);
+  layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2><hr></hr><h3> Airport name: " + feature.properties.name + "</h3>");
+  }
+}).addTo(map);
+
+});
